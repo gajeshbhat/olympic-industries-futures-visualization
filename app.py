ef393137b5e546072de1c42602b20f630f54ad7d
Gajesh Bhat <16153045+gajeshbhat@users.noreply.github.com>
2023-02-22 18:30:14 +0530

    Added Final Templates

--- app.py
+++ app.py
@@ -3,17 +3,13 @@ from process_data import read_data_from_db
 
 app = Flask(__name__)
 
-# Define route for the chart page
+# Define route for the index page
 @app.route("/")
-def chart():
-
+def index():
     # Get the selected chart column from the form data, or use the default values
     selected_column = request.args.get("columns", "open")
-
     chart_data = read_data_from_db(selected_column)
-    print(chart_data["labels"])
-    print(type(chart_data["labels"]))
-
+    
     return render_template(
         "index.html",
         labels=chart_data["labels"],
--- process_data.py
+++ process_data.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 import pandas as pd
 
+
 # Check if the database already exists, otherwise create it
 def create_db_if_not_exists():
     if not os.path.exists("futures_data.db"):
@@ -10,12 +11,12 @@ def create_db_if_not_exists():
 
         # Create table to store the futures data
         c.execute(
-            """CREATE TABLE futures_data
+            """CREATE TABLE futures_data_default
                     (date text, open real, high real, low real, close real, adj_close real, volume real)"""
         )
         futures_data = read_data_from_excel()
 
-        futures_data.to_sql("futures_data", conn, if_exists="replace", index=False)
+        futures_data.to_sql("futures_data_default", conn, if_exists="replace", index=False)
 
         # Close the database connection
         conn.close()
@@ -29,16 +30,16 @@ def read_data_from_db(column):
     # Connect to the database and fetch the data to plot
     conn = sqlite3.connect("futures_data.db")
 
-    query = "SELECT date, {0} FROM futures_data".format(column)
+    query = "SELECT date, {0} FROM futures_data_default".format(column)
     data = pd.read_sql(query, conn)
-
-    # Close the database connection
-    conn.close()
 
     # Generate the chart
     labels = data["date"].tolist()
     values = data[column].tolist()
-    columns = ['open', 'high', 'low', 'close', 'adj_close', 'volume']
+    columns = ["open", "high", "low", "close", "adj_close", "volume"]
+
+     # Close the database connection
+    conn.close()
 
     # List of supported chart types for the data
 
@@ -51,21 +52,16 @@ def read_data_from_db(column):
 
 
 def clean_data(futures_data):
-    # Drop the rows with missing or duplicate data
     futures_data = futures_data.dropna()
-    # Drop the rows with duplicate dates but keep the row with volume > 0
-    futures_data = futures_data.drop_duplicates(subset="date", keep="last")
-    # Remove the rows with any column containing a value '-'
     futures_data = futures_data[~futures_data.applymap(lambda x: x == "-").any(1)]
-
+    futures_data.columns = [
+        column.lower().strip().replace("*", "").replace("**", "").replace(" ", "_")
+        for column in futures_data.columns
+    ]
     return futures_data
 
 # Read the futures data from the Excel file and store it in the database
 def read_data_from_excel():
     futures_data = pd.read_excel("futures_data.xlsx")
-    futures_data.columns = [
-        column.lower().strip().replace("*", "").replace("**", "").replace(" ", "_")
-        for column in futures_data.columns
-    ]
     futures_data = clean_data(futures_data)
     return futures_data
